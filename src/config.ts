67f42bff17445fb3c65a0964131da84d1a2917bc
kevelscy <kevinblanco@turpialdev.com>
2022-10-27 17:16:23 -0400

    cleaning-code

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,4 @@
-import type{ NavItems } from './types'
-
-export const NAV_ITEMS: NavItems = {
-  home: { path: '/', title: 'home' },
-  about: { path: '/about', title: 'about' }
-}
-
 export const SITE = {
-  NAME: 'Kevin Blanco',
   TITLE: 'Kevin Blanco | Frontend Developer',
   DESCRIPTION: 'Desarrollo de productos web con Javascript, Typescript, React, Next, Node, Express y MongoDB.',
   URL: 'https://kevelscy.ml',
@@ -14,5 +6,3 @@ export const SITE = {
   AUTHOR: 'Kevin Blanco',
   KEYWORDS: 'Frontend Developer, Kevin Blanco, Kevelscy, Kevin Blanco Frontend, Kevin Blanco Portafolio, Frontend Venezuela'
 }
-
-export const PAGE_SIZE = 1
--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,3 @@
-export interface Project {
-	title: string;
-	client: string;
-	description: string;
-	publishDate: string;
-	tags: string[];
-	img: string;
-}
-
-export type NavItem = {
-  path: '/' | '/about',
-  title: string
-}
-
-export type NavItems = {
-  [key: string]: NavItem
-}
-
 export interface IIconBasicProps {
   classes?: string
 }
